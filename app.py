8579ba84e911a0fdb9be11c01d61a4aace972bd4
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2021-07-15 10:54:31 +0530

    alert button added

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask,render_template, redirect,url_for,send_file,request
+from flask import Flask,render_template, redirect,url_for,send_file,request,flash
 import os,sys
 from pydub import AudioSegment
 from pydub.utils import which
 
 app=Flask(__name__)
+app.secret_key="8f2cfea6b4c54e5b9cb21f6f70b7b8ce"
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -27,12 +28,20 @@ def upload_audio():
             if request.files:
                 if request.form['format'] == 'wav':
                     song = AudioSegment.from_file(os.path.join(app.config["UPLOADS"], audio.filename)).export("static/converted/converted.wav", format="wav")
+
                     print("DONE!")
-                    return send_file(app.config["CONVERTED"]+"/converted.wav", as_attachment=True)
+                    store= send_file(app.config["CONVERTED"]+"/converted.wav", as_attachment=True)
                     # return "converting....."
+                    #flash("Successfully Converted") 
+                    #return redirect(url_for("index"))
+                    return store
+
                 else:
                     return "Format Unsupported"
     except IsADirectoryError:
+        flash("Error occured, try again!")
         return redirect(url_for("index"))
+    #finally:
+          #flash("Successfully Converted....")
 if __name__ == '__main__':
     app.run(debug=True)
